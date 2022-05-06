5d2cc0165fb993831be5ca54b1806bd8a5488a66
skifzp <skifzp@gmail.com>
2022-05-06 12:05:00 +0300

    reformed code

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,9 +1,8 @@
 function getMinMax(str) {
   let arr = str.split(" ");
-  let numbers = arr.filter(item => !Number.isNaN(Number(item))).map(item => Number(item))
-    .sort( (a,b) => a-b);
+  let numbers = arr.filter(item => !Number.isNaN(Number(item))).map(item => Number(item)).sort( (a, b) => a - b );
   return {
     min: numbers[0],
-    max: numbers[numbers.length-1],
+    max: numbers[numbers.length - 1],
   }
 }
