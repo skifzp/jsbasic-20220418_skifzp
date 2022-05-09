a010d35ac8fc7a892afbdcca14153e710c762b6b
skifzp <skifzp@gmail.com>
2022-05-09 11:15:52 +0300

    done 4-module/1-task makeFriendsList()

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+  for( let friend of friends){
+    let li = document.createElement("li");
+    li.innerText = friend.firstName + " " + friend.lastName;
+    ul.appendChild(li);
+  }
+  return ul;
 }
