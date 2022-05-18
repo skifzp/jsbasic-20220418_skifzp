b754bafad5121745e19964660f4a7b2af82be0e0
skifzp <skifzp@gmail.com>
2022-05-18 12:02:22 +0300

    5-module/2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let toggleText = function(event){
+    let elemText = document.querySelector("#text");
+    elemText.hidden = !elemText.hidden;
+  }
+  let button = document.querySelector(".toggle-text-button");
+  button.addEventListener("click", toggleText);
 }
