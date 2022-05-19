1f3fb01cd2a642eb23e99e95f7a73e3c64e59e79
skifzp <skifzp@gmail.com>
2022-05-19 14:23:49 +0300

    5-module/3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,51 @@
 function initCarousel() {
-  // ваш код...
+  class Menu {
+    constructor(elem) {
+      this._elem = elem;
+      this.curNumberOfImage = 1;
+      this.elemArrowRight = elem.querySelector(".carousel__arrow_right");
+      this.elemArrowLeft = elem.querySelector(".carousel__arrow_left");
+      this.elemArrowLeft.style.display = 'none';
+      this.elemCarouselInner = elem.querySelector(".carousel__inner");
+      this.shift = this.elemCarouselInner.offsetWidth;
+      elem.onclick = this.onClick.bind(this);
+    }
+
+    right() {
+      if ( this.curNumberOfImage === 1 ) {
+        this.elemArrowLeft.style.display = '';
+      }
+      if ( this.curNumberOfImage < 4 ) {
+        this.elemCarouselInner.style.transform = 'translateX(-'+ this.shift * this.curNumberOfImage +'px)';
+        this.curNumberOfImage++;
+        if ( this.curNumberOfImage === 4 ) {
+          this.elemArrowRight.style.display = 'none';
+        }
+      }
+    }
+
+    left() {
+      if ( this.curNumberOfImage === 4 ) {
+        this.elemArrowRight.style.display = '';
+      }
+      if ( this.curNumberOfImage > 1 ) {
+        this.curNumberOfImage--;
+        this.elemCarouselInner.style.transform = 'translateX(-'+ this.shift * (this.curNumberOfImage-1) +'px)';
+        if ( this.curNumberOfImage === 1 ) {
+          this.elemArrowLeft.style.display = 'none';
+        }
+      }
+    }
+
+    onClick(event) {
+      let elemClassName = event.target.parentElement.className;
+      if (elemClassName.includes("carousel__arrow_right")) {
+        this["right"]();
+      } else if (elemClassName.includes("carousel__arrow_left")) {
+        this["left"]();
+      }
+    }
+  }
+
+  new Menu(document.querySelector(".carousel"));
 }
