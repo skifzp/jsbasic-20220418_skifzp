062a0a4f92f795569ea2c662fab90d41a1a8f3e2
skifzp <skifzp@gmail.com>
2022-05-24 23:01:18 +0300

    done 6-module/1+2 tasks

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,29 @@
  *
  */
 export default class UserTable {
+  elem;
   constructor(rows) {
+    this.elem = this['createTable'](rows);
+    document.onclick = this.onClick.bind(this); 
+  }
+
+  createTable(users){
+    let rows = "";
+    for ( let user of users ) {
+      rows += `<tr id="${user.name}"><td>${user.name}</td><td>${user.age}</td><td>${user.salary}</td><td>${user.city}</td><td><button>X</button></td></tr>`;
+    }
+    let html =  "<table><thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead>"
+            + "<tbody>" + rows + "</tbody></table>";
+    let e = document.createElement('html');
+    e.innerHTML = html;
+    return e;
+  }
+
+  onClick(event) {
+    let tagName = event.target.tagName;
+    if (tagName != 'BUTTON') {
+      return;
+    }
+    let rowId = event.target.parentElement.parentElement.remove();
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,40 @@
+import createElement from '../../assets/lib/create-element.js';;
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.elem = this.render();
+    this.elem.addEventListener("click", this.onClick.bind(this));
+  }
+
+  render(){
+    let html = 
+    `<div class="card" id="${this.product.id}">
+      <div class="card__top">
+        <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
+        <span class="card__price">€${(+(this.product.price)).toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">${this.product.name}</div>
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>`;
+    return createElement(html);
+  }
+
+  onClick(event) {
+    let tagName = event.target.className;
+    if (tagName != 'card__button') {
+      return;
+    }
+
+    event.target.dispatchEvent(
+                  new CustomEvent("product-add", 
+                  { // имя события должно быть именно "product-add"
+                    detail: this.product.id, // Уникальный идентификатора товара из объекта товара
+                    bubbles: true, // это событие всплывает - это понадобится в дальнейшем
+                  }));
   }
 }
